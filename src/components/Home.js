264b8fe11b2e53c95b901684e9ec994a4db3a80a
Anuj Chopra <anujchopra100@gmail.com>
2023-08-14 02:35:37 +0530

    create-homepage

---
+++ src/components/Home.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <>
+      <div className="home">
+        <img src="home-banner.gif" className="banner-image" alt="" />
+      </div>
+      <div className="card-container">
+        <div className="card">
+          <div className="card-image-container">
+            <img src="1.gif"  className="card-image" alt="" />
+          </div>
+          <Link to="/blogs" className="button-link"><button class="button">
+            <span>Grand Prize</span>
+          </button></Link> 
+          
+        </div>
+        <div className="card">
+          <div className="card-image-container">
+          <img src="2.gif" className="card-image" alt="" />
+          </div>
+          <Link to="/blogs" className="button-link"><button class="button">
+            <span>1st Prize</span>
+          </button></Link>
+        </div>
+        <div className="card">
+          <div className="card-image-container">
+          <img src="3.gif" className="card-image" alt="" />
+          </div>
+        
+          <Link to="/blogs" className="button-link"><button class="button">
+            <span>2nd Prize</span>
+          </button></Link>
+        </div>
+        <div className="card">
+          <div className="card-image-container">
+          <img src="4.gif" className=" card-image" alt="" />
+          </div>
+          <Link to="/blogs" className="button-link"><button class="button">
+            <span>3rd Prize</span>
+          </button></Link>
+        </div>
+        <div className="inner-container">
+          <div className="card">
+            <div className="card-image-container">
+            <img src="5.gif" className="card-image" alt="" />
+            </div>
+            <Link to="/blogs" className="button-link"><button class="button">
+              <span>4rth Prize</span>
+            </button></Link>
+          </div>
+          <div className="card">
+            <div className="card-image-container">
+            <img src="6.gif" className=" card-image" alt="" />
+            </div>
+            <Link to="/blogs" className="button-link"><button class="button">
+              <span>5th Prize</span>
+            </button></Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
