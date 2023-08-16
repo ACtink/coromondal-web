264e275e7e4a0a276cd80b904f369f9795a2c98e
Anuj Chopra <anujchopra100@gmail.com>
2023-08-16 11:17:02 +0530

    update grandpage background images

--- src/components/GrandPrize.js
+++ src/components/GrandPrize.js
@@ -38,10 +38,10 @@ function GrandPrize() {
         </Link>
         </div>
       <div className="grandPrize">
-        <img src="/images/Grand_prize_1.gif" className="grandprize-image" alt="" />
+        <img src="/images/main_1.gif" className="grandprize-image" alt="" />
       </div>
       <div className="grandprize-down-portion">
-         <img className="grand-down-image" src="/images/Grand_prize_2.gif" alt="" />
+         <img className="grand-down-image" src="/images/main_2.gif" alt="" />
       <div className="items-container">
         {isCardVisible && (
           <div className="grand-page-card-container">
@@ -75,7 +75,7 @@ function GrandPrize() {
 
         {
             showWinners && (
-                <div className={`transition-element ${isShowing ? 'show' : ''}`} >
+                <div className={` ${isShowing ? 'show' : ''}`} >
                      <img src="grandprizeWinners.gif" className="grand-winners-image" alt="grand-prize-announcement" />
                 </div>
             )
