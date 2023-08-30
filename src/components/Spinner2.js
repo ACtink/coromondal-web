9f0daca45fd637d9627ad47171fa8d4edef191a3
Anuj Chopra <anujchopra100@gmail.com>
2023-08-30 12:17:25 +0530

    update setimeout function

--- src/components/Spinner2.js
+++ src/components/Spinner2.js
@@ -2,48 +2,42 @@ import React, { useContext, useState } from "react";
 import "../spinner.css";
 import { ShowNewGrandPageContext } from "./GrandPageContext";
 
-function Spinner2({isSecondSpinnerVisible,setIsSecondSpinnerVisible, showSecondSelected , setShowSecondSelected , setIsShowing}) {
-
-
-    const [showSecondGrandPage, setShowSecondGrandPage] = useContext(ShowNewGrandPageContext);
-
+function Spinner2({
+  isSecondSpinnerVisible,
+  setIsSecondSpinnerVisible,
+  showSecondSelected,
+  setShowSecondSelected,
+  setIsShowing,
+}) {
+  const [showSecondGrandPage, setShowSecondGrandPage] = useContext(
+    ShowNewGrandPageContext
+  );
 
   const [animationClass, setAnimationClass] = useState("");
-  
 
-  console.log(isSecondSpinnerVisible , setIsSecondSpinnerVisible)
+  console.log(isSecondSpinnerVisible, setIsSecondSpinnerVisible);
 
   const handleSpinnerClick = () => {
-
     setAnimationClass("spinner-spin");
 
+    setTimeout(() => {
+      setAnimationClass("");
+      setIsSecondSpinnerVisible(false);
+      setShowSecondSelected(true);
+      setIsShowing(true);
+      setShowSecondGrandPage(true);
+    }, 10000);
 
-setTimeout(()=>{
-    setAnimationClass("")
-    setIsSecondSpinnerVisible(false)
-    setShowSecondSelected(true)
-    setIsShowing(true)
-    setShowSecondGrandPage(true)
-    
-
-
-    
-
-},4000)
-
-   // Apply the animation class
-    console.log(animationClass)
+    // Apply the animation class
+    console.log(animationClass);
   };
   return (
-    
     <>
       <div className="spinner-container" onClick={handleSpinnerClick}>
-    
         <img
           src="spinphoto.png"
           className={`spinner ${animationClass}`}
           alt="spin"
-        
         />
       </div>
     </>
